d0d9fb6b25bc2011de3d18d2a3baef1ea9d75520
sigpher <425223341@qq.com>
2023-05-05 21:50:48 +0800

    the start of trait

--- ch05/src/lib.rs
+++ ch05/src/lib.rs
@@ -41,3 +41,20 @@ impl Rectangle {
         self.width > other.width && self.height > other.height
     }
 }
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} {}", self.headline, self.author, self.location)
+    }
+}
--- ch09/src/main.rs
+++ ch09/src/main.rs
@@ -1,22 +1,27 @@
-use std::{
-    fs::File,
-    io::{ErrorKind, Write},
-};
+use std::error::Error;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     // let v = vec![1, 2, 3];
     // let ret = v.get(100).unwrap_or(&0);
     // println!("{}",ret);
-    let fs = File::open("hello.txt");
-    let mut file = match fs {
-        Ok(f) => f,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            },
-            other_error => panic!("Problem opening the file: {:?}", other_error),
-        },
-    };
-    file.write(b"buf").unwrap();
+    // let fs = File::open("hello.txt");
+    // let mut file = match fs {
+    //     Ok(f) => f,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file: {:?}", e),
+    //         },
+    //         other_error => panic!("Problem opening the file: {:?}", other_error),
+    //     },
+    // };
+    // file.write(b"buf").unwrap();
+    let text = "how wolrd";
+    let last_char = last_char_of_first_line(text);
+    println!("{:?}", last_char);
+    Ok(())
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
